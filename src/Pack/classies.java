392b30e141bf8240d97949353496f0e979fede04
leaved <noternen@yandex.ru>
2014-09-08 18:01:55 +0600

    12345678

--- src/Pack/classies.java
+++ src/Pack/classies.java
@@ -6,7 +6,11 @@ public class classies
     public static void main(String args[ ])
     {
         System.out.println( "Welcome to the world of Java");
+
+
         System.out.println( "And Work");
+
+        System.out.println( "And Work infinitely...");
 
 
     }
